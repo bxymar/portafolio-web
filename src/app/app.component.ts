d317956dd23b261c0c666458f308961fe93a882b
Beymar Vargas <vargasgutierrezbeymaredwin@gmail.com>
2025-02-13 21:08:57 -0400

    Primera Version del Portafolio  Web

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  imports: [MatSlideToggleModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'portafolio-web';
+}
