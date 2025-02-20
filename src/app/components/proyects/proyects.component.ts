fe44a6b11c46e6746bffc9a02541a839b59d99c8
Beymar Vargas <vargasgutierrezbeymaredwin@gmail.com>
2025-02-20 17:30:25 -0400

    Se agrego y modifico el componente de Proyectos

--- src/app/components/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-proyects',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './proyects.component.html',
   styleUrl: './proyects.component.scss'
 })
